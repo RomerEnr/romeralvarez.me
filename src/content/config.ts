cd913b37c1eb42a36dff9bff148cc538d535d1d5
“Romer” <romeralvareziv@gmail.com>
2023-02-25 22:37:21 +0000

    lot of changes

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,10 +1,12 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+
+const asir = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
+		image: z.string().optional(),
 		// Transform string to Date object
 		pubDate: z
 			.string()
@@ -18,4 +20,44 @@ const blog = defineCollection({
 	}),
 });
 
-export const collections = { blog };
+const python = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		image: z.string().optional(),
+		// Transform string to Date object
+		pubDate: z
+			.string()
+			.or(z.date())
+			.transform((val) => new Date(val)),
+		updatedDate: z
+			.string()
+			.optional()
+			.transform((str) => (str ? new Date(str) : undefined)),
+		heroImage: z.string().optional(),
+	}),
+});
+
+const bash = defineCollection({
+	// Type-check frontmatter using a schema
+	schema: z.object({
+		title: z.string(),
+		description: z.string(),
+		image: z.string().optional(),
+		// Transform string to Date object
+		pubDate: z
+			.string()
+			.or(z.date())
+			.transform((val) => new Date(val)),
+		updatedDate: z
+			.string()
+			.optional()
+			.transform((str) => (str ? new Date(str) : undefined)),
+		heroImage: z.string().optional(),
+	}),
+});
+
+
+
+export const collections = { asir, python, bash };
